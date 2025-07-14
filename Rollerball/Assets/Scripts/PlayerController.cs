7ee971ec63118e6a8eedf0848ce6644955f884b3
karl-arndt <157114758+karl-arndt@users.noreply.github.com>
2025-07-13 22:59:50 -0400

    Add score and win message display

--- Rollerball/Assets/Scripts/PlayerController.cs
+++ Rollerball/Assets/Scripts/PlayerController.cs
@@ -1,17 +1,25 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject wintTextObject;
     private Rigidbody rb;
+    private int count;
     private float movementX;
     private float movementY;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        wintTextObject.SetActive(false);
+        SetCountText();
     }
 
     void OnMove(InputValue movementValue)
@@ -19,6 +27,15 @@ public class PlayerController : MonoBehaviour
         Vector2 movementVector = movementValue.Get<Vector2>();
         movementX = movementVector.x;
         movementY = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 13)
+        {
+            wintTextObject.SetActive(true);
+        }
     }
 
     void FixedUpdate()
@@ -29,9 +46,11 @@ public class PlayerController : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("PickUp"))
+        if(other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
         }
         
     }
