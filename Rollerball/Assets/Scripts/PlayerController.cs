8766516d37a4cdb168c622bbdaf1009f509a4cb5
karl-arndt <157114758+karl-arndt@users.noreply.github.com>
2025-07-13 22:12:15 -0400

    Add collision detection to collectibles

--- Rollerball/Assets/Scripts/PlayerController.cs
+++ Rollerball/Assets/Scripts/PlayerController.cs
@@ -29,6 +29,10 @@ public class PlayerController : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        other.gameObject.SetActive(false);
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+        }
+        
     }
 }
